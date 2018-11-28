d515e40f052b9521b57a58762cd2355aa44b28d5
Devon Mack <devon.mack@shopify.com>
2018-11-28 14:38:22 -0500

    Fixed node server

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.static(__dirname));
 
 // routes
 
-app.get("/", function(req, res)) {
+app.get("/", function(req, res) {
   res.render("index");
-};
+});
 
 app.listen(port, function() {
   console.log("app running");
